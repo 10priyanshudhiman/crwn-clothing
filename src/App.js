b57cbc9923a59d9dc1e3abad6f496751fc75db31
Priyanshu Dhiman <10priyanshudhiman@gmail.com>
2021-04-05 23:18:29 +0530

    updating our shop page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Switch, Route,Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = (props) => {
  
   console.log(props);
   return(
   <div>
-    
     <h1> HATS PAGE </h1>
   </div>
   )
@@ -23,7 +23,7 @@ function App() {
     <div>
       <Switch>
       <Route exact path = '/' component = {HomePage}/>
-      <Route exact path='/hats' component = {HatsPage}/>
+      <Route exact path='/shop' component = {ShopPage}/>
       
       </Switch>
       
