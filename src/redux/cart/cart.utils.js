1cb9053eecd7af1309b0873dd1927875cd9f8c80
Priyanshu Dhiman <10priyanshudhiman@gmail.com>
2021-06-01 00:41:22 +0530

    added addItem and removeItem to checkout componnet

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,21 @@ export const addItemToCart  = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
+
+export const removeItemFromCart = (cartItems, cartItemtoRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id == cartItemtoRemove.id
+    );
+
+    if(existingCartItem.quantity == 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemtoRemove)
+    }
+
+    return cartItems.map(
+        cartItem =>
+        cartItem.id == cartItemtoRemove.id 
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    )
+
+}
